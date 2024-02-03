fd554ad9fae5134052883467d6b5f9ed08c553ec
dongkyun2331 <dongkyun2331@gmail.com>
2024-02-04 03:35:20 +0900

    softwareDesign2

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ function softwareDesign() {
       }
       if (input === "2") {
         console.log("정답");
+        softwareDesign2();
       }
       if (input === "3") {
         console.log("오답");
@@ -55,6 +56,31 @@ function softwareDesign() {
       }
       console.log(
         "순차 다이어그래은 행위 다어어그램이므로 동적이고, 순차적인 표현을 위한 다이어그램이다,"
+      );
+    }
+  );
+}
+
+function softwareDesign2() {
+  rl.question(
+    "2. 메시지 지향 미들웨어(Message-Oriented Middleware, MOM)에 대한 설명으로 틀린 것은?\n 1: 느리고 안정적인 응답보다는 즉각적인 응답이 필요한 온라인 업무에 적합하다.\n 2: 독립적인 애플리케이션을 하나의 통합된 시스템으로 묶기 위한 역할을 한다.\n 3: 송신측과 수신측의 연결 시 메시지 큐를 활용하는 방법이 있다\n 4: 상이한 애플리케이션 간 통신을 비동기 방식으로 지원한다.\n",
+    (input) => {
+      if (input === "1") {
+        console.log("정답");
+      }
+      if (input === "2") {
+        console.log("오답");
+      }
+      if (input === "3") {
+        console.log("오답");
+      }
+      if (input === "4") {
+        console.log("오답");
+      } else {
+        softwareDesign2();
+      }
+      console.log(
+        "MOM(Message Oriented Middleware)\n- 메시지 기반의 비동기형 메시지를 전달하는 방식의 미들웨어이다.\n- 온라인 업무보다는 이기종 분산 데이터 시스템의 데이터 동기를 위해 많이 사용한다.\n\nMoM(Message Oriented Middleware)은 즉각적인 응답을 원하는 경우가 아니라 다소 느리고 안정적인 응답을 필요로 하는 경우에 많이 사용됩니다."
       );
     }
   );
